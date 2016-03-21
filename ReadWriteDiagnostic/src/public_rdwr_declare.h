fb9bec3eed996e032f3803261c057e8558d18bf8
Steven R. Brandt <sbrandt@cct.lsu.edu>
2016-03-21 15:36:39 -0500

    Add declaration.

--- ReadWriteDiagnostic/src/public_rdwr_declare.h
+++ ReadWriteDiagnostic/src/public_rdwr_declare.h
@@ -2,5 +2,10 @@
 #define RDWR_DECLARE_H
 
 #define CCTKi_VarDataPtrI(CGH,TL,VI) RDWR_VarDataPtrI(CGH,TL,VI)
+extern
+#ifdef __cplusplus
+"C"
+#endif
+void *RDWR_VarDataPtrI(const cGH *,int,int);
 
 #endif
